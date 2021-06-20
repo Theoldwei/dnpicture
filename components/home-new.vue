992907832ed9ea5bfa5d944ea3100ffdad3f2f2a
Theoldwei <68727197+Theoldwei@users.noreply.github.com>
2021-06-20 20:38:34 +0800

    dnpicture

---
+++ components/home-new.vue
@@ -0,0 +1,26 @@
+<template>
+	<view>
+		3
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "home-new",
+		data() {
+			return {
+
+			};
+		},
+		mounted() {
+			// 修改页面的标题
+			uni.setNavigationBarTitle({
+				title: "最新"
+			})
+		}
+	}
+</script>
+
+<style>
+
+</style>
